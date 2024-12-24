5acead2e3d2966b7ee07387281f8c0de9490809e
Your Name <sakshyambanjade@gmail.com>
2024-12-24 19:57:36 +0545

    small changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { CContainer } from '@coreui/react';
+import Login from '../src/components/Login/Login';
+import Register from "../src/components/Register/Register"
 
-export default function App() {
+function App() {
   return (
-    <div>
-      <h1>hello world!</h1>
-    </div>
-  )
+    <Router>
+      <CContainer>
+        <Routes>
+          <Route path="/SignIn" element={<Login />} />
+          <Route path="/SignUp" element={<Register />} />
+        </Routes>
+      </CContainer>
+    </Router>
+  );
 }
+
+export default App;
